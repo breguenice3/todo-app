9f491010e3592e398be76464e3ed770f2625d1de
breguenice <breguenicedev@gmail.com>
2024-05-21 20:15:45 -0300

    finalizando a parte visual das tasks

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -1,23 +1,33 @@
+import { useEffect, useState, useRef } from 'react'
 import Item from '../item-list/item'
 import './style.css'
 import styled from 'styled-components'
 
 export default function Bgd() {
 
-    const todo = [
+    const todoInicial = useRef([
         {
+            id: 1,
             name: "Jog around the park 3x",
             completed: false
         },
         {
+            id: 2,
             name: "Complete online JavaScript course",
             completed: true
         },
         {
+            id: 3,
             name: "10 minutos meditation",
             completed: false
         }
-    ]
+    ]);
+
+    const [todo, setTodo] = useState(todoInicial.current);
+
+    useEffect(() => {
+        setTodo(todoInicial.current);
+    }, []);
 
     return (
         <div className='bg'>
@@ -27,15 +37,30 @@ export default function Bgd() {
                     <button className='theme'></button>
                 </div>
                 <DivInput>
-                    <input type="text" name="" id="" placeholder='Create a new todo...' />
-                    <button className='add'>Add</button>
+                    <input type="text" name="" id="txt" placeholder='Create a new todo...' />
+                    <button className='add' onClick={() => {
+                        const txt: any = document.getElementById('txt');
+                        if (txt.value !== "") {
+                            const newTodo = {
+                                id: todo.length + 1,
+                                name: txt.value,
+                                completed: false
+                            };
+                            setTodo([...todo, newTodo]);
+                            todoInicial.current.push(newTodo); 
+                            
+                        } else { 
+                            console.log('errora');
+                        }
+                        txt.value = ""
+                    }}>Add</button>
                 </DivInput>
             </div>
             <List>
                 <Itens>
                     {todo.map((item) => {
                         return (
-                            <Item name={item.name} />
+                            <Item key={item.id} name={item.name} />
                         )
                     })}
                 </Itens>
@@ -67,7 +92,7 @@ const List = styled.div`
     height: 500px;
     width: 520px;
     background-color: white;
-    border-radius: 15px;
+    border-radius: 5px;
 `
 
 const Itens = styled.div`
