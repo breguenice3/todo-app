4870e4b8c922a6e2cb9f67858482f30562de5025
breguenice <breguenicedev@gmail.com>
2024-05-28 13:13:26 -0300

    finalizando logica das tarefas

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -5,6 +5,34 @@ import styled from 'styled-components'
 
 export default function Bgd() {
 
+    const [theme, setTheme] = useState('white');
+    const [themeLetter, setThemeLetter] = useState('black');
+
+    const List = styled.div`
+        height: 500px;
+        width: 520px;
+        background-color: ${theme};
+        border-radius: 5px;
+        box-shadow: 0 0 0.1em black;
+    `
+
+    const DivInput = styled.div`
+        display: flex;
+        align-items: center;
+        gap: 20px;
+
+            & input{
+                background-color: ${theme};
+                box-shadow: 0 0 0.1em black;
+                color: ${themeLetter}
+            }
+
+            & button{
+                background-color: ${theme};
+                color: ${themeLetter};
+            }
+    `
+
     const todoInicial = useRef([
         {
             id: 1,
@@ -29,12 +57,32 @@ export default function Bgd() {
         setTodo(todoInicial.current);
     }, []);
 
+    console.log(todo[3]);
+    
+
     return (
-        <div className='bg'>
+        <div className='bg' id='bg'>
             <div className='container'>
                 <div className='header-container'>
                     <h1>T O D O</h1>
-                    <button className='theme'></button>
+                    <button id='button' className='theme' onClick={() => {
+                        if (theme == 'white') {
+                            setTheme('hsl(235, 24%, 19%)');
+                            setThemeLetter('white');
+                            document.getElementById('button')?.classList.add('theme2');
+                            document.getElementById('button')?.classList.remove('theme');
+                            document.getElementById('bg')?.classList.add('bg1');
+                            document.getElementById('bg')?.classList.remove('bg');
+
+                        } else {
+                            setTheme('white');
+                            setThemeLetter('black');
+                            document.getElementById('button')?.classList.remove('theme2');
+                            document.getElementById('button')?.classList.add('theme');
+                            document.getElementById('bg')?.classList.add('bg');
+                            document.getElementById('bg')?.classList.remove('bg1');
+                        }
+                    }}></button>
                 </div>
                 <DivInput>
                     <input type="text" name="" id="txt" placeholder='Create a new todo...' />
@@ -47,9 +95,8 @@ export default function Bgd() {
                                 completed: false
                             };
                             setTodo([...todo, newTodo]);
-                            todoInicial.current.push(newTodo); 
-                            
-                        } else { 
+                            todoInicial.current.push(newTodo);
+                        } else {
                             console.log('errora');
                         }
                         txt.value = ""
@@ -60,7 +107,7 @@ export default function Bgd() {
                 <Itens>
                     {todo.map((item) => {
                         return (
-                            <Item key={item.id} name={item.name}/>
+                            <Item key={item.id} name={item.name} completed={item.completed} bgcolor={theme} lettercolor={themeLetter} list={todoInicial} itemid={item.id} todo={todo} setTodo={setTodo}/>
                         )
                     })}
                 </Itens>
@@ -82,25 +129,14 @@ export default function Bgd() {
     )
 }
 
-const DivInput = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 20px;
-`
-
-const List = styled.div`
-    height: 500px;
-    width: 520px;
-    background-color: white;
-    border-radius: 5px;
-`
 
 const Itens = styled.div`
     height: 95%;
-    padding-top: 15px;
+    padding-top: 5px;
 `
 
 const Buttons = styled.div`
     display: flex;
     justify-content: space-between;
+    padding: 0px 5px;
 `
--- src/components/item-list/item.tsx
+++ src/components/item-list/item.tsx
@@ -1,45 +1,79 @@
 import styled from "styled-components"
-import { useState } from "react";
+import { useState, useEffect, ReactEventHandler } from "react";
+import './style.css'
 
 interface Props {
     name: string;
+    completed: boolean;
+    bgcolor: string;
+    lettercolor: string;
+    list: any;
+    itemid: number;
+    todo: any;
+    setTodo: any;
 }
 
+export default function Item({ name, completed, lettercolor, list, itemid, todo, setTodo }: Props) {
 
+    console.log(itemid);
 
-export default function Item({ name }: Props) {
-    const [a, setA] = useState('black');
-    const [line, setLine] = useState('none')
-    return (
-        <ItemDiv>
-            <div>
-                <StyledButton onClick={(obj) => {
-                    setA('gray');
-                    setLine('line-through')
-                }
-                }>
-                </StyledButton>
-                <p style={{ color: `${a}`, textDecoration: `${line}`}}>
-                    {name}
-                </p>
-            </div>
-            <hr />
-        </ItemDiv>
-    )
+    const handleClick = () => {
+        const updatedList = todo.map((item: any) => {
+            if (item.id === itemid) {
+                return { ...item, completed: true };
+            }
+            return item;
+        });
+        setTodo(updatedList);
+    };
+
+    if (completed) {
+        return (
+            <ItemDiv>
+                <div>
+                    <StyledButton>
+                    </StyledButton>
+                    <StyledP id={name} className="completed">
+                        {name}
+                    </StyledP>
+                </div>
+                <StyledHR />
+            </ItemDiv>
+        )
+    } else {
+        return (
+            <ItemDiv>
+                <div>
+                    <StyledButton onClick={handleClick}>
+                    </StyledButton>
+                    <StyledP id={name}>
+                        {name}
+                    </StyledP>
+                </div>
+                <StyledHR />
+            </ItemDiv >
+        )
+    }
 }
+
 
 const ItemDiv = styled.div`
-    width: 100%;
-    height: 50px;
-    background-color: white;
+width: 100%;
+height: 50px;
+display: flex;
+flex-direction: column;
+& div{
+    display: flex;
+    gap: 10px;
+    padding: 15px;
     align-items: center;
-    & div{
-        display: flex;
-        gap: 5px;
-        padding: 15px;
-        align-items: center;
-    }
+}
 `
+
+const StyledP = styled.p`
+
+`
+
 const StyledButton = styled.button`
     border-radius: 100%;
     width: 20px;
@@ -47,4 +81,11 @@ const StyledButton = styled.button`
     padding: 5px;
     border: 1px solid hsla(236.6666666666666, 9.090909090909099%, 61.1764705882353%, 0.295);
     background-color: transparent;
+    cursor: pointer;
+`
+
+const StyledHR = styled.hr`
+    background-color: white;
+    height: 1px;
+    border: 1px solid gray;
 `
